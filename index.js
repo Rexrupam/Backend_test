3f4493bdeab5aa280d847b0b15614dee816bb04f
rupam <rexrupam70@gmail.com>
2025-03-27 19:45:45 +0530

    github issue api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import axios from "axios"
 const app = express();
-
+app.use(express.json())
 dotenv.config({
     path: './.env'
 })
@@ -10,9 +10,10 @@ dotenv.config({
 const headers = {
     Authorization: `token ${process.env.GITHUB_TOKEN}`,
     Accept: "application/vnd.github.v3+json",
+    "Content-Type": "application/json"
   };
 
-app.get('/github', async(req,res)=>{
+app.get('/github/', async(req,res)=>{
     try {
         const userResponse = await axios.get(`${process.env.GITHUB_API_URL}/users/${process.env.GITHUB_USERNAME}`, { headers });
         const reposResponse = await axios.get(`${process.env.GITHUB_API_URL}/users/${process.env.GITHUB_USERNAME}/repos`, { headers });
@@ -28,7 +29,6 @@ app.get('/github', async(req,res)=>{
           })),
         });
       } catch (error) {
-        console.log(error)
         res.status(500).json({ error: error.message });
       }
 })
@@ -37,13 +37,30 @@ app.get('/github/:repoName', async(req,res)=>{
   try {
     const { repoName } = req.params
     const userResponse = await axios.get(`${process.env.GITHUB_API_URL}/repos/${process.env.GITHUB_USERNAME}/${repoName}`, { headers });
-    console.log(req.params)
     res.status(200).json(userResponse.data)
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 
 })
+
+app.post("/github/:repoName/issues", async (req, res) => {
+  
+  const { repoName } = req.params;
+  const { title, body } = req.body;
+  if (!title || !body) return res.status(400).json({ error: "Title and body are required." });
+
+  try {
+    const issueResponse = await axios.post(
+      `${process.env.GITHUB_API_URL}/repos/${process.env.GITHUB_USERNAME}/${repoName}/issues`,
+      { title, body },
+      { headers }
+    );
+    res.status(200).json({ response: issueResponse.data.url });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 app.listen(process.env.PORT || 9000, ()=>{
     console.log(`server is listening on ${process.env.PORT}`)
